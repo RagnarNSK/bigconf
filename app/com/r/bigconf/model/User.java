914a46c729b1e1fc627fe8f8c9bf855b1636fa08
Ragnar_NSK <ragnar.nsk@gmail.com>
2017-08-25 00:06:47 +0700

    conf process continued

--- app/com/r/bigconf/model/User.java
+++ app/com/r/bigconf/model/User.java
@@ -1,4 +1,6 @@
 package com.r.bigconf.model;
 
 public class User {
+    private int id;
+    private String name;
 }
--- app/com/r/bigconf/processing/ConfProcess.java
+++ app/com/r/bigconf/processing/ConfProcess.java
@@ -2,10 +2,19 @@ package com.r.bigconf.processing;
 
 import com.r.bigconf.model.Conference;
 
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConfProcess implements Runnable {
 
     private final Conference conference;
     public boolean isActive;
+
+    private ConferenceChannelsData active;
+    private ConferenceChannelsData building;
+
+    private Map<Integer, ByteBuffer> incoming = new HashMap<>();
 
     public ConfProcess(Conference conference) {
         this.conference = conference;
@@ -13,9 +22,35 @@ public class ConfProcess implements Runnable {
 
     @Override
     public void run() {
-        while (isActive){
+        while (isActive) {
+            if (System.currentTimeMillis() % conference.getRecordInterval() == 0) {
+                Map<Integer, ByteBuffer> incomingBuffer = new HashMap<>(incoming);
+                incoming.clear();
+                building = new ConferenceChannelsData();
+                incomingBuffer.forEach((id,bytes) -> {
 
+                });
+            }
         }
 
+    }
+
+    public ByteBuffer getForUser(int userId) {
+        if (active != null) {
+            ByteBuffer special = active.audioChannels.get(userId);
+            return special != null ? special : active.commonChannel;
+        } else {
+            return null;
+        }
+    }
+
+    private static class ConferenceChannelsData {
+        /**
+         * Audio channels map
+         * key - integer - user id
+         * value - wav content of conference WITHOUT source of user identified by key
+         */
+        private Map<Integer, ByteBuffer> audioChannels = new HashMap<>();
+        private ByteBuffer commonChannel;
     }
 }
