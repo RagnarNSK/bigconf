61de1413a6380932113cf8c7dc9f311b33555d20
ragnar <ragnar.nsk@gmail.com>
2018-04-21 01:15:08 +0700

    frontend continued

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,11 +1,45 @@
-export class UsersList  {
-
+const listItemTemplate = `<li >{{name}}</li>`;
+export class UsersList {
     constructor(container) {
         this.container = container;
+        this.url = bigconfRestRoutes.usersList;
     }
-
     init() {
-        $(this.container).append("Initialized");
+        let container = this.container;
+        let self = this;
+        $(container).append("Initializing");
+        $.getJSON(this.url).done(function (data) {
+            $(container).empty();
+            data.forEach(function (user) {
+                let filledTemplate = listItemTemplate.replace(/{{name}}/g, user.name);
+                let element = $(filledTemplate);
+                element.click(function () {
+                    self.onclick(user.id)
+                });
+                $(container).append(element)
+            })
+        });
+    }
+    onclick(id) {
+        console.log("user " + id + "clicked");
+    }
+}
+
+
+const myUserTemplate = `Hello {{name}}`;
+export class MyUserComponent {
+    constructor(container) {
+        this.container = container;
+        this.url = bigconfRestRoutes.usersMe;
+    }
+    init(){
+        let container = this.container;
+        let self = this;
+        $.getJSON(this.url).done(function (data) {
+            $(container).empty();
+            let filledTemplate = myUserTemplate.replace(/{{name}}/g, data.name);
+            $(container).append(filledTemplate);
+        });
     }
 
 }
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,16 @@
-import {UsersList} from './home.js';
+import {UsersList, MyUserComponent} from './home.js';
 import {TestConf} from "./testConf.js";
 
 $.when($.ready).then(function () {
-   let usersList = $("#root").append('<div id="UsersList"></div>');
-   new UsersList(usersList).init();
+    let usersList = $(`<div id="UsersList"></div>`);
+    let myUser = $(`<div id="MyUser"></div>`);
 
-   new TestConf().init();
+    let root = $("#root");
+    root.append(myUser);
+    root.append(usersList);
+
+    new MyUserComponent(myUser).init();
+    new UsersList(usersList).init();
+
+    new TestConf().init();
 });
